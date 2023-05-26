64b1b69e0210de3ca86a0f7d33b952a983022077
Cerulime <1124775300@qq.com>
2023-05-26 21:12:52 +0800

    Version 2

--- Analysis.py
+++ Analysis.py
@@ -44,11 +44,48 @@ def check(data,window_size,rank):
 		if abs(y[m]-y1)>args.check:
 			print(str(i)+"-> raw: "+str(y[m])+" prediction: "+str(y1))
 	return
+def fit(s,t,x,y):
+    tempx=[]
+    tempy=[]
+    for i in range(0,len(x)):
+        if s<=x[i] and x[i]<=t:
+            tempx.append(x[i])
+            tempy.append(y[i])
+    z=np.polyfit(tempx,tempy,1)
+    p=np.poly1d(z)
+    print(p)
+    yvals=np.polyval(z,tempx)
+    r2=1-np.sum((tempy-yvals)**2)/np.sum((tempy-np.mean(tempy))**2)
+    print(r2)
+    return tempx,yvals
+def logfit(s,t,x,y):
+    tempx=[]
+    tempy=[]
+    for i in range(0,len(x)):
+        if s<=x[i] and x[i]<=t:
+            tempx.append(x[i]-s)
+            tempy.append(y[i])
+    flag=1
+    if tempy[0]<0:
+        flag=-1
+        tempy=[-item for item in tempy]
+    logy=np.log(tempy)
+    z=np.polyfit(tempx,logy,1)
+    print(z)
+    yvals=[np.exp(z[1])*np.exp(z[0]*item) for item in tempx]
+    fz=0
+    fm=0
+    for i in range(0,len(tempx)):
+        fz+=(tempy[i]-yvals[i])**2
+        fm+=(tempy[i]-np.mean(tempy))**2
+    r2=1-fz/fm
+    print(r2)
+    return [item+s for item in tempx],[flag*item for item in yvals]
 
 if __name__=="__main__":
 	parser=argparse.ArgumentParser()
 	parser.add_argument("-r","--raw",type=str,required=True,help="input the raw file path as str")
-	parser.add_argument("-cor","--cor",type=int,default=0,help="input the correction of the balance")
+	parser.add_argument("-cor","--cor",type=float,default=0.0,help="input the correction of the balance")
 	parser.add_argument("-f","--figure",action="store_true",help="input for a figure output")
 	parser.add_argument("-d","--detail",action="store_true",help="input for a detail output")
 	parser.add_argument("-c","--check",type=int,default=0,help="input limitation for checking input(Advice: 6)")
@@ -85,9 +122,28 @@ if __name__=="__main__":
 	plt.figure(1)
 	plt.xlabel("Time(s)")
 	plt.ylabel("Acceleration(m/s^2)")
-	plt.plot(x,y)
+	plt.plot(x,y,"r")
+	'''
+	print("linear fit1:")
+	x1,y1=fit(1.8,2.7,x,y)
+	plt.plot(x1,y1,"b")
+	print("linear fit2:")
+	x2,y2=fit(4.5,5.35,x,y)
+	plt.plot(x2,y2,"b")
+	print("linear fit3:")
+	x3,y3=fit(10.2,11,x,y)
+	plt.plot(x3,y3,"b")
+	print("linear fit4:")
+	x4,y4=fit(12.7,13.5,x,y)
+	plt.plot(x4,y4,"b")
+	print("logfit:")
+	x5,y5=logfit(13.5,14.75,x,y)
+	plt.plot(x5,y5,"b")
+	'''
 	plt.show()
 	
+	sys.exit(0)
+
 	plt.figure(2)
 	plt.xlabel("Time(s)")
 	plt.ylabel("Velocity(m/s)")
